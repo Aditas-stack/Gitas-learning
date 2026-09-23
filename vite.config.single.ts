77342d2ebbba80051e146da6ca393963c4b0fe09
Aditas-stack <rjkovlaakeem@gmail.com>
2026-09-23 14:27:29 +0100

    Add files via upload

---
+++ vite.config.single.ts
@@ -0,0 +1,17 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), viteSingleFile()],
+  resolve: {
+    alias: {
+      '@': path.resolve('.'),
+    },
+  },
+  build: {
+    outDir: 'standalone',
+  },
+});
